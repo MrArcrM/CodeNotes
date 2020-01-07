13cd784b1c161729d7e83579de01d95d6d8319a7
Qu Guo (FA Talent) <v-quguo@microsoft.com>
2020-01-07 16:11:15 +0800

    class-04

---
+++ lc-python/239.滑动窗口最大值.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=239 lang=python3
+#
+# [239] 滑动窗口最大值
+#
+
+# @lc code=start
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
+        d = deque()
+        output = []
+        for i, n in enumerate(nums):
+            while d and n > nums[d[-1]]:
+                d.pop()
+            d.append(i)
+            if d[0] == i - k:
+                d.popleft()
+            if i >= k - 1:
+                output.append(nums[d[0]])
+
+        return output
+
+# @lc code=end
---
+++ lc-python/84.柱状图中最大的矩形.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=84 lang=python3
+#
+# [84] 柱状图中最大的矩形
+#
+
+# @lc code=start
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0)
+        stack = [-1]
+        max_area = 0
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                max_area = max(max_area, h * w)
+            stack.append(i)
+        heights.pop()
+
+        return max_area
+
+
+# @lc code=end
+
