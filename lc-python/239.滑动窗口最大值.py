78ead2c695e6bbd7f9b9c0b5e3d6187ba64cccc6
Qu Guo (FA Talent) <v-quguo@microsoft.com>
2020-01-08 11:41:15 +0800

    re-84-239

--- lc-python/239.滑动窗口最大值.py
+++ lc-python/239.滑动窗口最大值.py
@@ -11,6 +11,7 @@ class Solution:
     def maxSlidingWindow(self, nums: 'List[int]', k: 'int') -> 'List[int]':
         d = deque()
         output = []
+
         for i, n in enumerate(nums):
             while d and n > nums[d[-1]]:
                 d.pop()
@@ -19,7 +20,7 @@ class Solution:
                 d.popleft()
             if i >= k - 1:
                 output.append(nums[d[0]])
-
+            
         return output
 
 # @lc code=end
